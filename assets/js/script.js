489d3227b0ef6caf4734af11cbdfe9b81b1c6155
Fábio Rocha <Fabiorocharb@gmail.com>
2023-08-27 23:20:39 -0300

    Starting project , mobile and menu

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const menu = document.getElementById("menu");
+  menu.classList.toggle('active');
+  const active = menu.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
+}
+btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener("touchstart", toggleMenu);
+
+
+const container = document.getElementById('minha-animacao');
+  
+  // Configurações da animação (substitua 'nome-do-arquivo.json' pelo nome do seu arquivo Lottie)
+  const animacao = lottie.loadAnimation({
+    container: container,
+    renderer: 'svg', // ou 'canvas' ou 'html'
+    loop: true, // Define se a animação deve ser reproduzida em loop
+    autoplay: true, // Define se a animação deve começar automaticamente
+    path: 'file:///C:/Users/FábioRocha/Desktop/AnalistaRocha/assets/image/programer.json' // Caminho para o arquivo JSON da animação
+  });
+  
