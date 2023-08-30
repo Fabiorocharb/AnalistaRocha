d25400db574c12874baf4d3e31403763c5cc85ce
Fábio Rocha <Fabiorocharb@gmail.com>
2023-08-30 00:30:23 -0300

    party 2 mobile done

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,20 +21,54 @@ const container = document.getElementById('animation-one');
   // Configurações da animação (substitua 'nome-do-arquivo.json' pelo nome do seu arquivo Lottie)
   const animacao = lottie.loadAnimation({
     container: container,
-    renderer: 'svg', // ou 'canvas' ou 'html'
+    renderer: 'svg',
     loop: true, // Define se a animação deve ser reproduzida em loop
     autoplay: true, // Define se a animação deve começar automaticamente
     path: 'assets/image/programer.json' // Caminho para o arquivo JSON da animação
   });
 
   const containertwo = document.getElementById('animation-two');
-  
-  // Configurações da animação (substitua 'nome-do-arquivo.json' pelo nome do seu arquivo Lottie)
   const animacaotwo = lottie.loadAnimation({
     container: containertwo,
-    renderer: 'svg', // ou 'canvas' ou 'html'
-    loop: true, // Define se a animação deve ser reproduzida em loop
-    autoplay: true, // Define se a animação deve começar automaticamente
-    path: 'assets/image/seta.json' // Caminho para o arquivo JSON da animação
+    renderer: 'svg', 
+    loop: true, 
+    autoplay: true, 
+    path: 'assets/image/seta.json' 
+  });
+
+  const containerthree = document.getElementById('animation-three');
+  
+  const animacaothree = lottie.loadAnimation({
+    container: containerthree,
+    renderer: 'svg', 
+    loop: true, 
+    autoplay: true, 
+    path: 'assets/image/seo.json' 
   });
   
+  const containerfour = document.getElementById('animation-four');
+  const animacaofour = lottie.loadAnimation({
+    container: containerfour,
+    renderer: 'svg', 
+    loop: true, 
+    autoplay: true, 
+    path: 'assets/image/responsible.json' 
+  });
+
+  const uiux = document.getElementById('ui-ux');
+  const animacaosix = lottie.loadAnimation({
+    container: uiux,
+    renderer: 'svg', 
+    loop: true, 
+    autoplay: true, 
+    path: 'assets/image/uiux.json' 
+  });
+ 
+  const rowss = document.getElementById('rows1');
+  const animacaoseven = lottie.loadAnimation({
+    container: rows1,
+    renderer: 'svg', 
+    loop: true, 
+    autoplay: true, 
+    path: 'assets/image/seta.json' 
+  });
