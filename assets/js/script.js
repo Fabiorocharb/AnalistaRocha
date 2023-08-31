0cb0d222e572896a460e68083957cac4c83fa5f3
Fábio Rocha <Fabiorocharb@gmail.com>
2023-08-31 00:14:44 -0300

    starting 3 part site

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,74 +1,100 @@
-const btnMobile = document.getElementById('btn-mobile');
+const btnMobile = document.getElementById("btn-mobile");
 
 function toggleMenu(event) {
-  if (event.type === 'touchstart') event.preventDefault();
+  if (event.type === "touchstart") event.preventDefault();
   const menu = document.getElementById("menu");
-  menu.classList.toggle('active');
-  const active = menu.classList.contains('active');
-  event.currentTarget.setAttribute('aria-expanded', active);
+  menu.classList.toggle("active");
+  const active = menu.classList.contains("active");
+  event.currentTarget.setAttribute("aria-expanded", active);
   if (active) {
-    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+    event.currentTarget.setAttribute("aria-label", "Fechar Menu");
   } else {
-    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+    event.currentTarget.setAttribute("aria-label", "Abrir Menu");
   }
 }
 btnMobile.addEventListener("click", toggleMenu);
 btnMobile.addEventListener("touchstart", toggleMenu);
 
+const container = document.getElementById("animation-prog");
 
-const container = document.getElementById('animation-one');
-  
-  // Configurações da animação (substitua 'nome-do-arquivo.json' pelo nome do seu arquivo Lottie)
-  const animacao = lottie.loadAnimation({
-    container: container,
-    renderer: 'svg',
-    loop: true, // Define se a animação deve ser reproduzida em loop
-    autoplay: true, // Define se a animação deve começar automaticamente
-    path: 'assets/image/programer.json' // Caminho para o arquivo JSON da animação
-  });
+// Configurações da animação (substitua 'nome-do-arquivo.json' pelo nome do seu arquivo Lottie)
+const animacao = lottie.loadAnimation({
+  container: container,
+  renderer: "svg",
+  loop: true, // Define se a animação deve ser reproduzida em loop
+  autoplay: true, // Define se a animação deve começar automaticamente
+  path: "assets/image/programer.json", // Caminho para o arquivo JSON da animação
+});
 
-  const containertwo = document.getElementById('animation-two');
-  const animacaotwo = lottie.loadAnimation({
-    container: containertwo,
-    renderer: 'svg', 
-    loop: true, 
-    autoplay: true, 
-    path: 'assets/image/seta.json' 
-  });
+const containertwo = document.getElementById("animation-seta");
+const animacaotwo = lottie.loadAnimation({
+  container: containertwo,
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "assets/image/seta.json",
+});
 
-  const containerthree = document.getElementById('animation-three');
-  
-  const animacaothree = lottie.loadAnimation({
-    container: containerthree,
-    renderer: 'svg', 
-    loop: true, 
-    autoplay: true, 
-    path: 'assets/image/seo.json' 
-  });
-  
-  const containerfour = document.getElementById('animation-four');
-  const animacaofour = lottie.loadAnimation({
-    container: containerfour,
-    renderer: 'svg', 
-    loop: true, 
-    autoplay: true, 
-    path: 'assets/image/responsible.json' 
-  });
+const containerthree = document.getElementById("animation-seo");
 
-  const uiux = document.getElementById('ui-ux');
-  const animacaosix = lottie.loadAnimation({
-    container: uiux,
-    renderer: 'svg', 
-    loop: true, 
-    autoplay: true, 
-    path: 'assets/image/uiux.json' 
+const animacaothree = lottie.loadAnimation({
+  container: containerthree,
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "assets/image/seo.json",
+});
+
+const containerfour = document.getElementById("animation-responsible");
+const animacaofour = lottie.loadAnimation({
+  container: containerfour,
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "assets/image/responsible.json",
+});
+
+const uiux = document.getElementById("animation-uxui");
+const animacaosix = lottie.loadAnimation({
+  container: uiux,
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "assets/image/uiux.json",
+});
+
+const rowss = document.getElementById("setatwo");
+const animacaoseven = lottie.loadAnimation({
+  container: setatwo,
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "assets/image/seta.json",
+});
+
+$(document).ready(function(){
+  const $carousel = $('.carousel');
+  const $dots = $('.dot');
+
+  // Verifique se é um dispositivo móvel antes de ativar o carrossel
+  if ($(window).width() <= 768) {
+    $carousel.slick({
+      dots: false,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      adaptiveHeight: true // Mantém a altura consistente para projetos de diferentes tamanhos
+    });
+  }
+
+  // Adicione um evento de clique às bolas indicadoras para dispositivos móveis
+  $dots.on('click', function() {
+    const slideIndex = $(this).data('slide');
+    $carousel.slick('slickGoTo', slideIndex);
   });
- 
-  const rowss = document.getElementById('rows1');
-  const animacaoseven = lottie.loadAnimation({
-    container: rows1,
-    renderer: 'svg', 
-    loop: true, 
-    autoplay: true, 
-    path: 'assets/image/seta.json' 
-  });
+});
+
+
+
+
